bf63484b450d2929877bbfc753e1128c6780cf8f
Abdulfatai Quadri Adewale <75619825+abujafar93@users.noreply.github.com>
2022-06-21 03:58:14 +0100

    update keyup event script

--- app.js
+++ app.js
@@ -38,16 +38,16 @@ list.addEventListener('click', e => {
 // keyup event
 const filterTodos = (term) => {
   Array.from(list.children)
-    .filter((todo) => !todo.textContent.includes(term))  
+    .filter((todo) => !todo.textContent.toLowerCase().includes(term)) 
     .forEach((todo) => todo.classList.add('filtered'));
 
     Array.from(list.children)
-    .filter((todo) => todo.textContent.includes(term))
+    .filter((todo) => todo.textContent.toLowerCase().includes(term))
     .forEach((todo) => todo.classList.remove('filtered'));
 };
 
 
 search.addEventListener('keyup', () =>{
-  const term = search.value.trim();
+  const term = search.value.trim().toLowerCase();
   filterTodos(term);
 });
